d3600d40fd3321b3b22910f062e22c764c85894c
dor4231 <dor4231@gmail.com>
2017-08-10 00:33:14 +0300

    Added Timer class

--- 06_timer/timer.rb
+++ 06_timer/timer.rb
@@ -1,3 +1,41 @@
 class Timer
-  #write your code here
+  attr_accessor :seconds
+  def initialize seconds=0
+    @seconds = seconds
+    @minutes = @seconds / 60
+    @hours = @minutes / 60
+  end
+
+  def seconds=(new_seconds)
+    @seconds = new_seconds
+    @minutes = @seconds / 60
+    @hours = @minutes / 60
+  end
+
+  def time_string
+    timeFormat = ["00", "00", "00"]
+    #Convert the number of seconds to minutes
+    if @seconds > 60
+      # If needed convert the number of minutes to hours.
+      if @minutes > 60
+        timeFormat[0] =  self.numberChanger(@hours)
+        @minutes -= @hours * 60
+
+      end
+      timeFormat[1] = self.numberChanger(@minutes)
+      @seconds -= @minutes * 60 + @hours * 3600
+    end
+    timeFormat[2] = self.numberChanger(@seconds)
+    timeFormat[0] + ":" + timeFormat[1] + ":" + timeFormat[2]
+  end
+
+  def numberChanger number 
+    number = number.to_s
+    if number.length > 1
+      return number
+    else
+      return "0" + number
+    end
+  end
+
 end
